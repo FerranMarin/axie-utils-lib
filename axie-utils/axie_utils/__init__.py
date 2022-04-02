46fa0bd58081dec496b5d2eb52172e82ef9da5cc
Ferran Marin <ferran.marin.llobet@gmail.com>
2022-04-02 02:50:32 +0200

    Bump version

--- axie-utils/axie_utils/__init__.py
+++ axie-utils/axie_utils/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.0.5'
+__version__ = '2.0.6'
 __all__ = [
     'Axies',
     'AxieGraphQL',
--- axie-utils/tests/test_module.py
+++ axie-utils/tests/test_module.py
@@ -2,7 +2,7 @@ import axie_utils
 
 
 def test_version():
-    assert axie_utils.__version__ == '2.0.5'
+    assert axie_utils.__version__ == '2.0.6'
 
 
 def test_init():
