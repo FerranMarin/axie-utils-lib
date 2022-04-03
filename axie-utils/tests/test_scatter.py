3bcd3adbb619d59722888877857f19b148e64f43
Ferran Marin <ferran.marin.llobet@gmail.com>
2022-04-03 15:02:36 +0200

    Fix tests

--- axie-utils/tests/test_scatter.py
+++ axie-utils/tests/test_scatter.py
@@ -1,34 +1,62 @@
+from subprocess import call
 from web3 import Web3
-from mock import patch
+from mock import patch, call
 
 from axie_utils import Scatter, TrezorScatter
+from axie_utils.utils import TOKEN, SCATTER_CONTRACT
 from tests.utils import MockedAllowed, MockedNotAllowed
     
 
-
-def test_scatter_init():
+@patch("web3.eth.Eth.contract")
+@patch("web3.Web3.toChecksumAddress", return_value='checksum')
+def test_scatter_init(mocked_checksum, mocked_contract):
     s = Scatter('slp', 'ronin:from_acc', '0xprivate_key', {'ronin:abc1': 1, 'ronin:dce2': 10})
     assert s.token == 'slp'
     assert s.from_acc == '0xfrom_acc'
     assert s.from_private == '0xprivate_key'
-    assert s.to_list == ['0xabc1', '0xdce2']
+    mocked_checksum.assert_has_calls(calls=[
+        call(TOKEN['slp']),
+        call(SCATTER_CONTRACT),
+        call('0xabc1'),
+        call('0xdce2')
+    ])
+    mocked_contract.assert_called()
+    assert s.to_list == ['checksum', 'checksum']
     assert s.amounts_list == [1, 10]
 
-def test_scatter_init_ron():
+
+@patch("web3.eth.Eth.contract")
+@patch("web3.Web3.toChecksumAddress", return_value='checksum')
+def test_scatter_init_ron(mocked_checksum, mocked_contract):
     s = Scatter('ron', 'ronin:from_acc', '0xprivate_key', {'ronin:abc1': 1, 'ronin:dce2': 10})
     assert s.token == 'ron'
     assert s.from_acc == '0xfrom_acc'
     assert s.from_private == '0xprivate_key'
-    assert s.to_list == ['0xabc1', '0xdce2']
+    mocked_checksum.assert_has_calls(calls=[
+        call(SCATTER_CONTRACT),
+        call('0xabc1'),
+        call('0xdce2')
+    ])
+    mocked_contract.assert_called()
+    assert s.to_list == ['checksum', 'checksum']
     assert s.amounts_list == [Web3.toWei(1, 'ether'), Web3.toWei(10, 'ether')]
 
 
-def test_scatter_init_trezor():
+@patch("web3.eth.Eth.contract")
+@patch("web3.Web3.toChecksumAddress", return_value='checksum')
+def test_scatter_init_trezor(mocked_checksum, mocked_contract):
     s = TrezorScatter('slp', 'ronin:from_acc', 'client', "m/44'/60'/0'/0/0", {'ronin:abc1': 1, 'ronin:dce2': 10})
     assert s.token == 'slp'
     assert s.from_acc == '0xfrom_acc'
     assert s.client == 'client'
-    assert s.to_list == ['0xabc1', '0xdce2']
+    mocked_checksum.assert_has_calls(calls=[
+        call(TOKEN['slp']),
+        call(SCATTER_CONTRACT),
+        call('0xabc1'),
+        call('0xdce2')
+    ])
+    mocked_contract.assert_called()
+    assert s.to_list == ['checksum', 'checksum']
     assert s.amounts_list == [1, 10]
 
 
@@ -96,29 +124,41 @@ def test_approve_contract_trezor(*args):
     assert r == True
 
 
+@patch("web3.eth.Eth.contract")
+@patch("web3.Web3.toChecksumAddress")
 @patch("axie_utils.scatter.get_nonce", return_value=1)
-def test_increase_gas_tx_no_avilable_nonce(mocked_nonce):
+def test_increase_gas_tx_no_avilable_nonce(mocked_nonce, mocked_checksum, mocked_contract):
     s = Scatter('slp', 'ronin:from_acc', '0xprivate_key', {'ronin:abc1': 1, 'ronin:dce2': 10})
     r = s.increase_gas_tx(123)
     mocked_nonce.assert_called()
+    mocked_checksum.assert_called()
+    mocked_contract.assert_called()
     assert r is None
 
 
+@patch("web3.eth.Eth.contract")
+@patch("web3.Web3.toChecksumAddress")
 @patch("axie_utils.scatter.Scatter.execute_token")
 @patch("axie_utils.scatter.get_nonce", return_value=123)
-def test_increase_gas_tx(mocked_nonce, mocked_execute_token):
+def test_increase_gas_tx(mocked_nonce, mocked_execute_token, mocked_checksum, mocked_contract):
     s = Scatter('slp', 'ronin:from_acc', '0xprivate_key', {'ronin:abc1': 1, 'ronin:dce2': 10})
     s.increase_gas_tx(123)
     mocked_nonce.assert_called()
+    mocked_checksum.assert_called()
+    mocked_contract.assert_called()
     mocked_execute_token.assert_called_with(1.01, 123)
 
 
+@patch("web3.eth.Eth.contract")
+@patch("web3.Web3.toChecksumAddress")
 @patch("axie_utils.scatter.Scatter.execute_ron")
 @patch("axie_utils.scatter.get_nonce", return_value=123)
-def test_increase_gas_tx_ron(mocked_nonce, mocked_execute_ron):
+def test_increase_gas_tx_ron(mocked_nonce, mocked_execute_ron, mocked_checksum, mocked_contract):
     s = Scatter('ron', 'ronin:from_acc', '0xprivate_key', {'ronin:abc1': 1, 'ronin:dce2': 10})
     s.increase_gas_tx(123)
     mocked_nonce.assert_called()
+    mocked_checksum.assert_called()
+    mocked_contract.assert_called()
     mocked_execute_ron.assert_called_with(1.01, 123)
 
 
